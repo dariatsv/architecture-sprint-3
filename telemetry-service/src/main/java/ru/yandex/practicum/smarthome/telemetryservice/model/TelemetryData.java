275593e42dffc944fa48949c0708df4866a234ed
Daria <dasha21a@icloud.com>
2024-10-22 21:29:41 +0300

    sprint 3

--- telemetry-service/src/main/java/ru/yandex/practicum/smarthome/telemetryservice/model/TelemetryData.java
+++ telemetry-service/src/main/java/ru/yandex/practicum/smarthome/telemetryservice/model/TelemetryData.java
@@ -1,13 +1,22 @@
 package ru.yandex.practicum.smarthome.telemetryservice.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
-@Table(name = "telemetry_data")
+@Table(name = "telemetry")
 public class TelemetryData {
 
     @Id
@@ -19,4 +28,20 @@ public class TelemetryData {
     private Double temperature;
 
     private LocalDateTime timestamp;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        TelemetryData that = (TelemetryData) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
